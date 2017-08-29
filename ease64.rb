8333b2760d61b1363c548365bbe1479cb6c7e862
esquinas <esquinas.enrique@gmail.com>
2017-08-29 18:14:01 +0200

    Fix grammar of "digits" and digit grouping. PASS

--- ease64.rb
+++ ease64.rb
@@ -2,16 +2,14 @@ module Ease64
   module_function
 
   def encode64(bin)
-    result = ''
-
     octals_blob = bin.bytes.map { |byte| byte.to_s(8) }.join
 
     # TODO: Optimize! if octals_blob.length % ?? ...
-    two_digits_octals = octals_blob.scan(/..?/).map { |octa|
+    two_digit_octals = octals_blob.scan(/..?/).map { |octa|
       octa.ljust(2, '0')
     }
 
-    result = two_digits_octals.map do |oo|
+    result = two_digit_octals.map do |oo|
       num = oo.oct
       (num < 63) ? (num + 64).chr : num.chr
     end
@@ -25,7 +23,8 @@ module Ease64
       octal.slice(-2..-1)
     }.join
 
-    three_digit_octals = octals_blob.scan(/..?./)
+    three_digit_octals = octals_blob.scan(/..?.?/)
+    three_digit_octals.pop if three_digit_octals.last.oct ==  0
 
     result = three_digit_octals.map { |ooo| ooo.oct.chr }
 
