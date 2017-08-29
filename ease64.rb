3c16759174f376fa35e096ed4c4fec5b89032138
esquinas <esquinas.enrique@gmail.com>
2017-08-29 18:05:53 +0200

    Implement decode64. PASS

--- ease64.rb
+++ ease64.rb
@@ -13,13 +13,22 @@ module Ease64
 
     result = two_digits_octals.map do |oo|
       num = oo.oct
-      (num < 63 ) ? (num + 64).chr : num.chr
+      (num < 63) ? (num + 64).chr : num.chr
     end
 
     result.join
   end
 
   def decode64(str)
-    'A' # TODO
+    octals_blob = str.chars.map { |c|
+      octal = c.ord.to_s(8)
+      octal.slice(-2..-1)
+    }.join
+
+    three_digit_octals = octals_blob.scan(/..?./)
+
+    result = three_digit_octals.map { |ooo| ooo.oct.chr }
+
+    result.join
   end
 end
