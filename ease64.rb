172e5615f285b1ba6758975296b79fcf7c153093
esquinas <esquinas.enrique@gmail.com>
2017-08-29 17:39:40 +0200

    Implement #encode64. PASS. 1 test skipped

--- ease64.rb
+++ ease64.rb
@@ -2,7 +2,21 @@ module Ease64
   module_function
 
   def encode64(bin)
-    'HH' # TODO
+    result = ''
+
+    octals_blob = bin.bytes.map { |byte| byte.to_s(8) }.join
+
+    # TODO: Optimize! if octals_blob.length % ?? ...
+    two_digits_octals = octals_blob.scan(/..?/).map { |octa|
+      octa.ljust(2, '0')
+    }
+
+    result = two_digits_octals.map do |oo|
+      num = oo.oct
+      (num < 63 ) ? (num + 64).chr : num.chr
+    end
+
+    result.join
   end
 
   def decode64(str)
