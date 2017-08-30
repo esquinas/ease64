f5d042959e37a563ebae7bf5aa0d8fab9d9e6e48
esquinas <esquinas.enrique@gmail.com>
2017-08-30 14:03:15 +0200

    Test for visible ascii chars and null chars. RED

--- ease64_test.rb
+++ ease64_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride' # Optional, can be commented out.
 require_relative 'ease64'
 
 class Ease63Test < Minitest::Test
+  # ### ENCODE TESTS
   def test_encode_one_char
     # skip
     enc = Ease64.encode64('A')
@@ -15,6 +16,7 @@ class Ease63Test < Minitest::Test
     assert_equal 'IAeMalMx', enc
   end
 
+  # ### DECODE TESTS
   def test_decode_one_char
     # skip
     plain = Ease64.decode64('HH')
@@ -25,5 +27,31 @@ class Ease63Test < Minitest::Test
     # skip
     plain = Ease64.decode64('IAeMalMx')
     assert_equal 'Hello', plain
+  end
+
+  # ### ENCODE + DECODE TESTS
+
+  def test_handle_one_null_char
+    # skip
+    plain = "\x00"
+    enc = Ease64.encode64(plain)
+    assert_equal plain, Ease64.decode64(enc)
+  end
+
+  def test_handle_null_chars
+    # skip
+    null_char = "\x00"
+    6.times do |n|
+      plain = null_char * (n + 1)
+      enc = Ease64.encode64(plain)
+      assert_equal plain, Ease64.decode64(enc)
+    end
+  end
+
+  def test_handle_visible_ascii_chars
+    # skip
+    ascii_string = [*' '..'~'].shuffle.join
+    enc = Ease64.encode64(ascii_string)
+    assert_equal ascii_string, Ease64.decode64(enc)
   end
 end
