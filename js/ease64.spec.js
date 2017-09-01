c5efd7ad4fc583e5e6f849e22f2b4a03727da18e
esquinas <esquinas.enrique@gmail.com>
2017-09-01 17:59:42 +0200

    Standard JS.

--- js/ease64.spec.js
+++ js/ease64.spec.js
@@ -1,9 +1,8 @@
-var Ease64 = require('./ease64');
+var Ease64 = require('./ease64')
 
-describe('Ease64 encoding', function() {
-
-  it('encodes one char', function() {
-    var e64 = new Ease64();
-    expect(ease64.encode64('A')).toBe('HH');
+describe('Ease64 encoding', function () {
+  it('encodes one char', function () {
+    var ease64 = new Ease64()
+    expect(ease64.encode64('A')).toBe('HH')
   })
 })
